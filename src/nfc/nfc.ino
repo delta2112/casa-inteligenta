297717bdb29206ebe41e22b4e13ff03a55ba67c4
Sebastian Ponovescu <delta2112@yahoo.com>
2020-02-27 09:05:29 +0200

    updated PIN definitions

--- src/nfc/nfc.ino
+++ src/nfc/nfc.ino
@@ -43,28 +43,6 @@ This is a MFRC522 library example; for further details and other examples see: h
 
 ///////////////////////////////////////// Setup ///////////////////////////////////
 void setup() {
-  //Arduino Pin Configuration
-  pinMode(redLed, OUTPUT);
-  pinMode(greenLed, OUTPUT);
-  pinMode(blueLed, OUTPUT);
-  pinMode(wipeB, INPUT_PULLUP);   // Enable pin's pull up resistor
-  pinMode(relay, OUTPUT);
-  //Be careful how relay circuit behave on while resetting or power-cycling your Arduino
-  digitalWrite(relay, HIGH);    // Make sure door is locked
-  digitalWrite(redLed, LED_OFF);  // Make sure led is off
-  digitalWrite(greenLed, LED_OFF);  // Make sure led is off
-  digitalWrite(blueLed, LED_OFF); // Make sure led is off
-
-  //Protocol Configuration
-  Serial.begin(9600);  // Initialize serial communications with PC
-  SPI.begin();           // MFRC522 Hardware uses SPI protocol
-  mfrc522.PCD_Init();    // Initialize MFRC522 Hardware
-
-  //If you set Antenna Gain to Max it will increase reading distance
-  //mfrc522.PCD_SetAntennaGain(mfrc522.RxGain_max);
-
-  Serial.println(F("Access Control Example v0.1"));   // For debugging purposes
-  ShowReaderDetails();  // Show details of PCD - MFRC522 Card Reader details
 
   //Wipe Code - If the Button (wipeB) Pressed while setup run (powered on) it wipes EEPROM
   if (digitalRead(wipeB) == LOW) {  // when button pressed pin should get low, button connected to ground
